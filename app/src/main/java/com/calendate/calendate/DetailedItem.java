2feecb9cb9bf90661a28e291bb79295e9c861bd0
hilayc9 <hilayc9@gmail.com>
2017-06-27 16:25:05 +0300

    add facebook login

--- app/src/main/java/com/calendate/calendate/DetailedItem.java
+++ app/src/main/java/com/calendate/calendate/DetailedItem.java
@@ -19,6 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
@@ -33,7 +34,7 @@ public class DetailedItem extends AppCompatActivity implements View.OnClickListe
     LocalDateTime date;
     FirebaseDatabase mDatabase;
     FirebaseUser user;
-    String key;
+    String key = "";
     int hours = 0, minutes = 0;
     String btnRef;
     int fragNum;
@@ -84,7 +85,9 @@ public class DetailedItem extends AppCompatActivity implements View.OnClickListe
     }
 
     private void readOnce() {
-        mDatabase.getReference("events/" + user.getUid() + "/" + btnRef + fragNum).addListenerForSingleValueEvent(new ValueEventListener() {
+        DatabaseReference ref = mDatabase.getReference();
+        ref = mDatabase.getReference("events/" + user.getUid() + "/" + btnRef + fragNum);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
